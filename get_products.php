d3e1a3f20a8a20a8ba5ff19b94b9059da969927d
Janco-work <grobbelaarjanco@gmail.com>
2025-06-12 12:38:19 +0200

    Delete get_products.php

--- get_products.php
+++
@@ -1,7 +0,0 @@
-<?php
-require '../php/db.php';
-header('Content-Type: application/json');
-// Only select products that are NOT sold
-$stmt = $pdo->query("SELECT id, name, description, price, image, email, created_at FROM products WHERE sold = 0");
-echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
-?>
